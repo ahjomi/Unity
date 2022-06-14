2fed4daa41dae1ceabb91785cd5c251917aea9b1
Jeong Min Ahn <ehdaorwn3192@gmail.com>
2022-06-14 11:45:25 +0900

    Descriptions added

--- BackgroundScroller.cs
+++ BackgroundScroller.cs
@@ -7,23 +7,27 @@ public class BackgroundScroller : MonoBehaviour
     public Transform BG1, BG2;
     public float scrollSpeed;
     
+    // How wide is the image
     private float bgWidth;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Look at the BG1 SpriteRenderer and tells how wide the image is 
         bgWidth = BG1.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+      // Multiply Time.deltaTime for constant speed regardless of what PC you are running 
       BG1.position = new Vector3(BG1.position.x - (scrollSpeed * Time.deltaTime), BG1.position.y, BG2.position.z);
       BG2.position -= new Vector3(scrollSpeed * Time.deltaTime, 0f, 0f);
 
+      // -bgWidth - 1 = 26
       if(BG1.position.x < -bgWidth - 1 )
       {
+          // Move back over to the right
           BG1.position += new Vector3(bgWidth * 2f, 0f, 0f);
       }
       if(BG2.position.x < -bgWidth - 1 )
--- PlayerController.cs
+++ PlayerController.cs
@@ -16,17 +16,28 @@ public class PlayerController : MonoBehaviour
     public float timeBetweenShots = .1f;
     private float shotCounter;
 
-    // Start is called before the first frame update
+    // Start is called before the first frame update (This will start as soons as the game starts)
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    // Update is called once per frame (It happens repeatedly over and over again 
+    // 60fpx: Update will happen 60times per sec)
     void Update()
     {
+        /* 
+            new Vector2 = A variable that can have a x & y value
+            x = Input.GetAxisRaw("Horizontal") 
+            Input = Default input system (Player presses a key)
+            GetAxisRaw("Horizontal") = Unity's default axies (Move left and right) if a player 
+            presses a button like a or d it gives a # to represnet that(left -1, right +1)
+            y = Input.GetAxisRaw("Vertical") 
+        */
        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
-
+        /* 
+            new Vector3 = z,y and x 
+        */
        transform.position = new Vector3(
            Mathf.Clamp(transform.position.x, bottomLeftLimit.position.x, topRightLimit.position.x),
            Mathf.Clamp(transform.position.y, bottomLeftLimit.position.y, topRightLimit.position.y),
